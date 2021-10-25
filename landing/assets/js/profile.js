99d5de9dabe5865c4337d0123478879497d7775f
jfiora <sillonpeludo@gmail.com>
2021-10-25 10:03:07 -0300

    remove node modules

--- landing/assets/js/profile.js
+++ landing/assets/js/profile.js
@@ -1,5 +1,6 @@
 Parse.initialize('Dk6gGgFPeXajSp1dYqjuYGpBdApowPJFvDQyHAZ6', 'zGTJyFjUyW2yL6eiCCQNvKBoNNPW8j6utVd5XwnG');
 Parse.serverURL = 'https://parseapi.back4app.com/';
+let nameP1, nameP2, nameP3, nameP4, nameP5, nameP6, name1MP, name2MP, name3MP, titleP1, titleP2, titleP3, titleP4, titleP5, titleP6, title1MP, title2MP, title3MP;
 
 async function retrievePerson() {
     let Person = Parse.Object.extend('');
@@ -7,40 +8,40 @@ async function retrievePerson() {
 
     try {
         let person1 = query.get('KRVXr4bFYE');
-        let nameP1 = person1.get('name');
-        let titleP1 = person1.get('title');
+        nameP1 = person1.get('name');
+        titleP1 = person1.get('title');
         
         let person2 = query.get('GNHw1BaRCh');
-        let nameP2 = person2.get('name');
-        let titleP2 = person2.get('title');
+        nameP2 = person2.get('name');
+        titleP2 = person2.get('title');
         
         let person3 = query.get('pLH9DCPcUr');
-        let nameP3 = person3.get('name');
-        let titleP3 = person3.get('title');
+        nameP3 = person3.get('name');
+        titleP3 = person3.get('title');
         
         let person4 = query.get('LRvi8rMPUh');
-        let nameP4 = person4.get('name');
-        let titleP4 = person4.get('title');
+        nameP4 = person4.get('name');
+        titleP4 = person4.get('title');
         
         let person5 = query.get('2XDdwHbV22');
-        let nameP5 = person5.get('name');
-        let titleP5 = person5.get('title');
+        nameP5 = person5.get('name');
+        titleP5 = person5.get('title');
         
         let person6 = query.get('ocmZMmjfxM');
-        let nameP6 = person6.get('name');
-        let titleP6 = person6.get('title');
+        nameP6 = person6.get('name');
+        titleP6 = person6.get('title');
 
         let person1MP = query.get('2WxWqYIXxj');
-        let name1MP = person1MP.get('name');
-        let title1MP = person1MP.get('title');
+        name1MP = person1MP.get('name');
+        title1MP = person1MP.get('title');
 
         let person2MP = query.get('wKYgnsGFOe');
-        let name2MP = person2MP.get('name');
-        let title2MP = person2MP.get('title');
+        name2MP = person2MP.get('name');
+        title2MP = person2MP.get('title');
 
         let person3MP = query.get('HAadNTMZiy');
-        let name3MP = person3MP.get('name');
-        let title3MP = person3MP.get('title');
+        name3MP = person3MP.get('name');
+        title3MP = person3MP.get('title');
     } catch (err) {
         console.log('retrieve failed: ', err);
     }
